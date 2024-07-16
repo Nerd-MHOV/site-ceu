3031dbfccc145eb668633b797af5d53b9a8c1225
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2024-07-16 17:32:54 -0300

    teste glb

--- src/app/estrutura/teste/page.tsx
+++ src/app/estrutura/teste/page.tsx
@@ -5,16 +5,19 @@ import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import CanvasLoader from '@/components/ui/Loader'
+import { positions } from "@mui/system"
 
 const TestePage = () => {
     return <Layout>
         <div className="mt-[120px] min-h-screen">
-            <Canvas className='!min-h-screen '>
+            <Canvas camera={ {
+                position: [0,20,-20],
+            } } style={{ height: 800 }} className='min-h-screen h-screen'>
                 <ambientLight intensity={1.5} />
                 <directionalLight intensity={1.5} />
-                <OrbitControls />
+                <OrbitControls autoRotate />
                 <Suspense fallback={<CanvasLoader />}>
-                    <Model />
+                    <Model position={[10,0,-40]}/>
                 </Suspense>
             </Canvas>
 
